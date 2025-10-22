cd57b10ebf84c28d33488a1dce01c59be6ef5155
Robben1972 <bozorovshahob27@gmail.com>
2025-10-23 00:31:51 +0500

    phone not unique

--- users/models.py
+++ users/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import Group, Permission
 
 class CustomUser(AbstractUser):
     email = models.EmailField(unique=True)
-    phone_number = models.CharField(max_length=15, null=True, blank=True)
+    phone_number = models.CharField(max_length=15, null=True, blank=True, unique=False)
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
     role = models.CharField(max_length=20, default='client')
